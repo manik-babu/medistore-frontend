024ba577cf173e5900a40f40740a2ffdc4e4d46f
manik-babu <mdmanikbabu48@gmail.com>
2026-01-29 23:28:09 +0600

    Add environment variables and make variables type with zod and t3-oss

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,12 +1,13 @@
+import { env } from "@/env";
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: env.BACKEND_URL
 })
 
 export const signInWithGoogle = async () => {
     authClient.signIn.social({
         provider: "google",
-        callbackURL: "http://localhost:3000"
+        callbackURL: env.FRONTEND_URL
     });
 }
